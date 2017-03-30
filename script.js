cf965ac70ff05e5b05145d164e50b6926fb7b513
Ben Parsons <ben@bpulse.co.uk>
2017-03-30 22:22:50 +0100

    render the blocks and adjust when expanding years/months

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 var CELL_HEIGHT = 18;
+var monarchs = [];
+var monarchs_url = "http://baroque.bpulse.co.uk/index.php?title=Special:Ask/format%3Djson/link%3Dall/sort%3DAscended/order%3DASC/headers%3Dshow/searchlabel%3DJSON/class%3Dsortable-20wikitable-20smwtable/offset%3D/limit%3D50/-5B-5BCategory:English-20Monarchs-5D-5D/-3FAscended/-3FDescended/mainlabel%3D/prettyprint%3Dtrue/unescape%3Dtrue";
 
 $(document).ready(function() {
   renderDateLabels();
@@ -28,10 +30,47 @@ function renderDateLabels() {
 
     $("#timeline").append(year_block);
   }
+
+  getMonarchData();
+}
+
+function getMonarchData() {
+  $.getJSON(monarchs_url, function(data) {
+    for (var i in data.results) {
+      var obj = data.results[i];
+      if (obj.printouts.Ascended[0] && obj.printouts.Descended[0]) {
+        monarchs[obj.fulltext] = {
+          Ascended: new Date(parseInt(obj.printouts.Ascended[0].timestamp, 10) * 1000),
+          Descended: new Date(parseInt(obj.printouts.Descended[0].timestamp, 10) * 1000)
+        };
+      }
+    }
+    renderMonarchRanges();
+  });
+}
+
+function renderMonarchRanges() {
+  $(".monarch_div").remove();
+  for (var i in monarchs) {
+    var ascended_year = monarchs[i].Ascended.getFullYear();
+    var ascended_month = monarchs[i].Ascended.getMonth() + 1;
+    var startPosition = getMonthTopPosition(ascended_year, ascended_month);
+
+    var descended_year = monarchs[i].Descended.getFullYear();
+    var descended_month = monarchs[i].Descended.getMonth() + 1;
+    var endPosition = getMonthTopPosition(descended_year, descended_month) + CELL_HEIGHT;
+
+    var monarch_div = $("<div>").text(i);
+    monarch_div.css('top', startPosition);
+    monarch_div.css('height', endPosition - startPosition);
+    monarch_div.addClass('monarch_div');
+    $("#timeline").append(monarch_div);
+  }
 }
 
 function yearClick(yearLabel) {
   $(yearLabel.target).siblings(".month_list").toggle();
+  renderMonarchRanges();
 }
 
 function getMonthTopPosition(year, month) {
